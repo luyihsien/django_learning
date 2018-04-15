f60cbb74773431176321a69f190bcf09987a2706
luyihsien@gmail.com <luyihsien>
2018-04-15 02:39:19 -0700

    django basic project about website

--- mblog/settings.py
+++ mblog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q4%ba6n7-s9q5ovl=a68hp%mw6s8p@fmkz$oywy$$o6^&f0*e('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
